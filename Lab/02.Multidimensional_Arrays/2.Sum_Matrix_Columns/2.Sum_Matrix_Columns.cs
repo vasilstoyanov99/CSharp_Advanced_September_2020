3b8be40188d333240f0e8c96e697c34145910795
Vasil Stoyanov <acbfr@gmail.com>
2020-09-17 19:07:22 +0300

    Add homework

---
+++ Lab/02.Multidimensional_Arrays/2.Sum_Matrix_Columns/2.Sum_Matrix_Columns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _1.Sum_Matrix_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] data = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rows = data[0];
+            int columns = data[1];
+            int[,] matrix = new int[rows, columns];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = GetData();
+
+                for (int col = 0; col < columns; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            for (int col = 0; col < columns; col++)
+            {
+                int currColumnSum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    currColumnSum += matrix[row, col];
+                }
+
+                Console.WriteLine(currColumnSum);
+            }
+        }
+
+        static int[] GetData()
+        {
+            return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+    }
+}
