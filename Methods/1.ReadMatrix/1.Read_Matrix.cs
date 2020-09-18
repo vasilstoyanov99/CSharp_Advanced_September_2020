0fe01cd717b40710c2a5a0af9464b6d3c489be48
Vasil Stoyanov <acbfr@gmail.com>
2020-09-18 19:40:05 +0300

    Add new methods

---
+++ Methods/1.ReadMatrix/1.Read_Matrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+
+namespace _1.ReadMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = ReadMatrix(3, 6);
+        }
+        static int[,] ReadMatrix(int rows, int cols)
+        {
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] currentRow = GetRowData();
+
+                for (int col = 0; col < currentRow.Length; col++)
+                {
+                    matrix[row, col] = currentRow[col];
+                }
+            }
+
+            return matrix;
+        }
+        static int[] GetRowData()
+        {
+            char[] separetors = new char[] { ' ', ',' };
+            return Console.ReadLine().Split(separetors, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+    }
+}
---
+++ Methods/2.Print_Matrix/2.Print_Matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _2.Print_Matrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
