92540020d7be6fab50b45a4266da443877dd0c60
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-23 11:12:41 +0300

    Add one more method

---
+++ Methods/4.Print_Jagged_Array/4.Print_Jagged_Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _4.Print_Jagged_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void PrintArray(int[][] array)
+        {
+            for (int row = 0; row < array.GetLength(0); row++)
+            {
+                Console.WriteLine(String.Join(" ", array[row]));
+            }
+        }
+    }
+}
