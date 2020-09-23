eaf24109485d677314f379cca16d1ead8df181cb
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-23 11:10:39 +0300

    Add one more method

---
+++ Methods/3.Read_Jagged_Array/3.Read_Jagged_Array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.Read_Jagged_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] array = FillJaggedArray(rows);
+        }
+        static int[][] FillJaggedArray(int rows)
+        {
+            int[][] array = new int[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                array[row] = numbers;
+            }
+
+            return array;
+        }
+    }
+}
