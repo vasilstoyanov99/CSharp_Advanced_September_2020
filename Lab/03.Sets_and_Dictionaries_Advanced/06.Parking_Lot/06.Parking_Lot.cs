f5da09022339712dfd9575d265cf987a69d2640e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-26 12:59:42 +0300

    Add done homework

---
+++ Lab/03.Sets_and_Dictionaries_Advanced/06.Parking_Lot/06.Parking_Lot.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            HashSet<string> carNumbers = new HashSet<string>();
+
+            while (input != "END")
+            {
+                string[] data = input.Split(", ");
+
+                switch (data[0])
+                {
+                    case "IN":
+                        carNumbers.Add(data[1]);
+                        break;
+                    case "OUT":
+                        carNumbers.Remove(data[1]);
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (carNumbers.Count > 0)
+            {
+                foreach (var number in carNumbers)
+                {
+                    Console.WriteLine(number);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
