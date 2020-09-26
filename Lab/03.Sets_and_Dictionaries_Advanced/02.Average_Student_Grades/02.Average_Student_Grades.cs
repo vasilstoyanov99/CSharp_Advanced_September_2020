f7425bb378a6c14d1b91c15e4ac629eb3d404270
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-26 12:09:46 +0300

    Add done homework

---
+++ Lab/03.Sets_and_Dictionaries_Advanced/02.Average_Student_Grades/02.Average_Student_Grades.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Average_Student_Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> studentsData = new Dictionary<string, List<double>>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] studentInfo = Console.ReadLine().Split();
+                string studentName = studentInfo[0];
+                double grade = double.Parse(studentInfo[1]);
+
+                if (!studentsData.ContainsKey(studentName))
+                {
+                    studentsData.Add(studentName, new List<double>());
+                    studentsData[studentName].Add(grade);
+                }
+                else
+                {
+                    studentsData[studentName].Add(grade);
+                }
+            }
+
+            foreach (var student in studentsData)
+            {
+                StringBuilder result = new StringBuilder();
+                result.Append($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    result.Append($"{grade:f2} ");
+                }
+
+                result.Append($"(avg: {student.Value.Average():f2})");
+                Console.WriteLine(result.ToString());
+            }
+        }
+    }
+}
