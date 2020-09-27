10baba9c4d0f9930f087d290fe5adaad5c533724
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-27 11:51:16 +0300

    Add done homework

---
+++ Exercise/03.Sets_and_Dictionaries_Advanced/02.Sets_of_Elements/02.Sets_of_Elements.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Sets_of_Elements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] setsSize = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            HashSet<int> n = new HashSet<int>();
+            HashSet<int> m = new HashSet<int>();
+
+            for (int i = 0; i < setsSize[0]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                n.Add(number);
+            }
+
+            for (int i = 0; i < setsSize[1]; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                m.Add(number);
+            }
+
+            var numbersInBothSets = new List<int>(); 
+
+            foreach (var numberOne in n)
+            {
+                foreach (var numberTwo in m)
+                {
+                    if (numberOne == numberTwo)
+                    {
+                        Console.Write($"{numberOne} ");
+                    }
+                }
+            }
+        }
+    }
+}
