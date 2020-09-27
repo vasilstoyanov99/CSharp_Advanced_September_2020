cb33747d1012a254f0ace46b2f73957d2d7095b1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-27 12:28:16 +0300

    Add done homework

---
+++ Exercise/03.Sets_and_Dictionaries_Advanced/05.Count_Symbols/05.Count_Symbols.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            var timesOfOccurrences = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char currChar = text[i];
+
+                if (!timesOfOccurrences.ContainsKey(currChar))
+                {
+                    timesOfOccurrences.Add(currChar, 1);
+                }
+                else
+                {
+                    timesOfOccurrences[currChar]++;
+                }
+            }
+
+            foreach (var currChar in timesOfOccurrences)
+            {
+                Console.WriteLine($"{currChar.Key}: {currChar.Value} time/s");
+            }
+        }
+    }
+}
