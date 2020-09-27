1a509e2461c953eb9fcdf58591e81b166d208e2e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-27 11:50:42 +0300

    Add done homework

---
+++ Exercise/03.Sets_and_Dictionaries_Advanced/01.Unique_Usernames/01.Unique_Usernames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.Unique_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            HashSet<string> uniqueUsernames = new HashSet<string>();
+            
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string username = Console.ReadLine();
+                uniqueUsernames.Add(username);
+            }
+
+            foreach (var username in uniqueUsernames)
+            {
+                Console.WriteLine(username);
+            }
+        }
+    }
+}
