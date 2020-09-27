44430ab1a60bb750bde77aad94e8949510a3b9d4
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-27 12:23:04 +0300

    Add done homework

---
+++ Exercise/03.Sets_and_Dictionaries_Advanced/04.Even_Times/04.Even_Times.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Even_Times
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            var numbers = new Dictionary<int, int>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                int currNumber = int.Parse(Console.ReadLine());
+
+                if (!numbers.ContainsKey(currNumber))
+                {
+                    numbers.Add(currNumber, 1);
+                }
+                else
+                {
+                    numbers[currNumber]++;
+                }
+            }
+
+            foreach (var currNumber in numbers)
+            {
+                if (currNumber.Value % 2 == 0)
+                {
+                    Console.WriteLine(currNumber.Key);
+                    break;
+                }
+            }
+        }
+    }
+}
