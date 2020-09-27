ec0335ecff675644a8f6560219459917f31a7645
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-27 12:16:55 +0300

    Add done homework

---
+++ Exercise/03.Sets_and_Dictionaries_Advanced/03.Periodic_Table/03.Periodic_Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Periodic_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+            var uniqueChemicalElements = new SortedSet<string>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                string[] elements = Console.ReadLine().Split();
+
+                for (int k = 0; k < elements.Length; k++)
+                {
+                    uniqueChemicalElements.Add(elements[k]);
+                }
+            }
+
+            foreach (var elements in uniqueChemicalElements)
+            {
+                Console.Write(elements + " ");
+            }
+        }
+    }
+}
