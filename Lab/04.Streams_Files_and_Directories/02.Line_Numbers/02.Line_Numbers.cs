717513a1c0fa56123f5240b471aaea67d073d8e0
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-30 12:50:31 +0300

    Add done homework

---
+++ Lab/04.Streams_Files_and_Directories/02.Line_Numbers/02.Line_Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace _02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../Text.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../Result.txt"))
+                {
+                    int lineIndex = 1;
+                    string read = reader.ReadLine();
+
+                    while (read != null)
+                    {
+                        writer.Write($"{lineIndex}. {read}");
+                        writer.Write(Environment.NewLine);
+                        read = reader.ReadLine();
+                        lineIndex++;
+                    }
+                }
+            }
+        }
+    }
+}
