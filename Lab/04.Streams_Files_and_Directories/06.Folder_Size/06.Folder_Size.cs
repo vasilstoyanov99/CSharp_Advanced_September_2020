59de2233b81c10387f90ddcb5e692098e2dfd1c7
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-30 16:58:32 +0300

    Add done homework

---
+++ Lab/04.Streams_Files_and_Directories/06.Folder_Size/06.Folder_Size.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace _06.Folder_Size
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directoryPath = Console.ReadLine();
+            string[] files = Directory.GetFiles(directoryPath);
+            double sum = 0;
+
+            for (int currFile = 0; currFile < files.Length; currFile++)
+            {
+                FileInfo info = new FileInfo(files[currFile]);
+                Console.WriteLine(info.FullName);
+                sum += info.Length;
+            }
+
+            decimal MB = (decimal)(sum / 1000000.00);
+            Console.WriteLine($"Bytes: {sum}");
+            Console.WriteLine($"MB: {MB:f2}");
+        }
+    }
+}
