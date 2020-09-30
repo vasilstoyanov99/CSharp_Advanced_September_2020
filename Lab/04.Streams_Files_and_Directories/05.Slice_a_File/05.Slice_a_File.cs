75775b6258686f3201b8762d7cb756ab897befcb
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-30 14:10:43 +0300

    Add done homework

---
+++ Lab/04.Streams_Files_and_Directories/05.Slice_a_File/05.Slice_a_File.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace _05.Slice_a_File
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../sliceMe.txt"))
+            {
+                int partsCounter = 0;
+                string read = reader.ReadLine();
+
+                while (read != null)
+                {
+                    if (read != String.Empty)
+                    {
+                        partsCounter++;
+
+                        using (StreamWriter writer = new StreamWriter($"../../../Part-{partsCounter}.txt"))
+                        {
+                            writer.Write(read);
+                        }
+                    }
+
+                    read = reader.ReadLine();
+                }
+            }
+        }
+    }
+}
