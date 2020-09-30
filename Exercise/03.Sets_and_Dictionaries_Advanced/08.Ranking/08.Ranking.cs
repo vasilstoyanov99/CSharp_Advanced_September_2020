fbe9619fa783ec20d06f6fd739d29911d2d2264b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-30 12:30:11 +0300

    Add done homework

--- Exercise/03.Sets_and_Dictionaries_Advanced/08.Ranking/08.Ranking.cs
+++ Exercise/03.Sets_and_Dictionaries_Advanced/08.Ranking/08.Ranking.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 
 namespace _08.Ranking
 {
---
+++ Lab/04.Streams_Files_and_Directories/01.Odd_Lines/01.Odd_Lines.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace _01.Odd_Lines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader("../../../ToSplit.txt"))
+            {
+                using (StreamWriter writer = new StreamWriter("../../../OnlyOddLines.txt"))
+                {
+                    int linesCounter = 0;
+                    string read = reader.ReadLine();
+
+                    while (read != null)
+                    {
+                        linesCounter++;
+
+                        if (linesCounter % 2 == 1)
+                        {
+                            writer.Write(read);
+                            writer.Write(Environment.NewLine);
+                        }
+
+                        read = reader.ReadLine();
+                    }
+                }
+            } 
+        }
+    }
+}
