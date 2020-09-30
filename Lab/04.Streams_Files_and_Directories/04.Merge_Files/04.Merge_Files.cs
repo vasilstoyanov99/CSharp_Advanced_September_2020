c997137b1a51b2d0846f2a855aa0814ed35e5deb
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-09-30 13:58:51 +0300

    Add done homework

---
+++ Lab/04.Streams_Files_and_Directories/04.Merge_Files/04.Merge_Files.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace _04.Merge_Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader readerOne = new StreamReader("../../../FileOne.txt"))
+            {
+                using (StreamReader readerTwo = new StreamReader("../../../FileTwo.txt"))
+                {
+                    using (StreamWriter writer = new StreamWriter("../../../result.txt"))
+                    {
+                        string readFromOne = readerOne.ReadLine();
+                        string readFromTwo = readerTwo.ReadLine();
+                        bool shoudlCycleEnd = false;
+
+                        while (true)
+                        {
+                            if (readerOne != null)
+                            {
+                                writer.Write(readFromOne);
+
+                                if (!shoudlCycleEnd)
+                                {
+                                    writer.WriteLine();
+                                }
+                            }
+
+                            if (readerTwo != null)
+                            {
+                                writer.Write(readFromTwo);
+
+                                if (!shoudlCycleEnd)
+                                {
+                                    writer.WriteLine();
+                                }
+                            }
+                            
+                            readFromOne = readerOne.ReadLine();
+                            readFromTwo = readerTwo.ReadLine();
+
+                            if (shoudlCycleEnd)
+                            {
+                                break;
+                            }
+
+                            if (readerOne.EndOfStream == true && readerTwo.EndOfStream == true)
+                            {
+                                shoudlCycleEnd = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
