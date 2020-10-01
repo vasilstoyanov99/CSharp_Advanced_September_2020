e3b7c77a903e97e1e9c2b5c6e2ff4a8bde175104
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-01 20:38:26 +0300

    Add done homework

---
+++ Exercise/04.Streams_Files_and_Directories/02.Line_Numbers/02.Line_Numbers.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace _02.Line_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("../../../text.txt");
+            var result = new string[lines.Length];
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int lettersCount = GetLettersCount(lines[i]);
+                int punctuationMarksCount = GetPunctuationMarksCount(lines[i]);
+                result[i] = $"Line {i + 1}: -I was quick to judge him, but it wasn't his fault. ({lettersCount})({punctuationMarksCount})";
+            }
+
+            File.WriteAllLines("../../../output.txt", result);
+        }
+
+        static int GetLettersCount(string line)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currChar = line[i];
+
+                if (char.IsLetter(currChar))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+        static int GetPunctuationMarksCount(string line)
+        {
+            int counter = 0;
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char currChar = line[i];
+
+                if (char.IsPunctuation(currChar))
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
