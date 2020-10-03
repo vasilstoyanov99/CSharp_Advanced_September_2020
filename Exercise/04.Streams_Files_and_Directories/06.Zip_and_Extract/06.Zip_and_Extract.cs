6910b5808703740c4b328acd79e6a122771a7404
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-03 20:56:35 +0300

    Add done homework

---
+++ Exercise/04.Streams_Files_and_Directories/06.Zip_and_Extract/06.Zip_and_Extract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Compression;
+
+namespace _06.Zip_and_Extract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string archiveFrom = @"C:\Users\Vasko\Documents\FromFile";
+            string to = @"C:\Users\Vasko\Documents\ToFile\MyZipFile.zip";
+            ZipFile.CreateFromDirectory(archiveFrom, to);
+            string extractFrom = @"C:\Users\Vasko\Documents\ToFile\MyZipFile.zip";
+            string toArriveHere = @"C:\Users\Vasko\Documents\ToExtractHere\MyUnZipedFile";
+            ZipFile.ExtractToDirectory(extractFrom, toArriveHere);
+
+        }
+    }
+}
