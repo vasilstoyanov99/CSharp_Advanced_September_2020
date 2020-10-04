d5687b17262639f26b02f576a98d8c7a26a0c1c2
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-04 12:28:38 +0300

    Add done homework

---
+++ Lab/05.Functional_Programming/02.Sum_Numbers/02.Sum_Numbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Sum_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            PrintResult(GetArrayLenght, GetArraySum, numbers);
+        }
+
+        static int GetArrayLenght(int[] numbers)
+        {
+            return numbers.Length;
+        }
+
+        static int GetArraySum(int[] numbers)
+        {
+            return numbers.Sum();
+        }
+
+        static void PrintResult(Func<int[], int> lenght, Func<int[], int> sum, int[] numbers)
+        {
+            Console.WriteLine(lenght(numbers));
+            Console.WriteLine(sum(numbers));
+        }
+    }
+}
