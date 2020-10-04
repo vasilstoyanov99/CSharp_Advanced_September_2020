2305c7c7add42fa61f2caabdb02218d7cdd64d4a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-04 12:16:10 +0300

    Add done homework

---
+++ Lab/05.Functional_Programming/01.Sort_Even_Numbers/01.Sort_Even_Numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Sort_Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] evenNumbers = Console.ReadLine().Split(", ").Select(x => int.Parse(x)).Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+            Console.WriteLine(String.Join(", ", evenNumbers));
+        }
+    }
+}
