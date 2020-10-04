fcfac6f93ecb8e23dcdf8fb276f637090b7dda37
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-04 13:36:14 +0300

    Add done homework

---
+++ Lab/05.Functional_Programming/03.Count_Uppercase_Words/03.Count_Uppercase_Words.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03.Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                PrintResult(CheckIfWordStartsWithUppercase, words[i]);
+            }
+            
+        }
+
+        static void PrintResult(Func<string, bool> fucn, string word)
+        {
+            bool result = CheckIfWordStartsWithUppercase(word);
+
+            if (result)
+            {
+                Console.WriteLine(word);
+            }
+        }
+
+        static bool CheckIfWordStartsWithUppercase(string word)
+        {
+            if (char.IsLetter(word[0]))
+            {
+                return char.IsUpper(word[0]);
+            }
+
+            return false;
+        }
+    }
+}
