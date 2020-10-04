47125a82c8195b92622e372ffbca3bd0aa1c73e2
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-04 14:43:27 +0300

    Add done homework

---
+++ Lab/05.Functional_Programming/05.Filter_By_Age/05.Filter_By_Age.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _05.Filter_By_Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfLines = int.Parse(Console.ReadLine());
+            Person[] peopleData = new Person[numberOfLines];
+
+            for (int i = 0; i < numberOfLines; i++)
+            {
+                string[] data = Console.ReadLine().Split(", ");
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                peopleData[i] = new Person(name, age);
+            }
+
+            string condition = Console.ReadLine();
+            int ageToSort = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+            Func<Person, bool> sortByDelegate = SortBy(condition, ageToSort);
+            Action<Person> printerDelegate = PrintResults(format);
+
+            foreach (var person in peopleData)
+            {
+                if (sortByDelegate(person))
+                {
+                    printerDelegate(person);
+                }
+            }
+
+        }
+
+        static Func<Person, bool> SortBy(string condition, int age)
+        {
+            switch (condition)
+            {
+                case "younger":
+                    return p => p.Age < age;
+                case "older":
+                    return p => p.Age >= age;
+                default:
+                    return null;
+            }
+        }
+
+        static Action<Person> PrintResults(string format)
+        {
+            switch (format)
+            {
+                case "name":
+                    return p => Console.WriteLine($"{p.Name}");
+                case "age":
+                    return p => Console.WriteLine($"{p.Age}");
+                case "name age":
+                    return p => Console.WriteLine($"{p.Name} - {p.Age}");
+                default:
+                    return null;
+            }
+        } 
+    }
+
+    class Person
+    {
+       public string Name { get; set; }
+       public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+}
