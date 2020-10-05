1828d59c628426ae0b2d7b17aea46452c4a16e01
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-05 19:00:42 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/02.Knights_of_Honor/02.Knights_of_Honor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _02.Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Func<string, string> sirAdder = AddSir;
+            Action<string> printer = Print;
+
+            foreach (var name in names)
+            {
+                Print(AddSir(name));
+            }
+        }
+
+        static string AddSir(string name)
+        {
+            return "Sir " + name;
+        }
+
+        static void Print(string result)
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
