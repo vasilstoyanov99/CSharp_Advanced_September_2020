d477ce3b917a7b2061cb331c3331fae80264da2a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-05 19:05:25 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/03.Custom_Min_Function/03.Custom_Min_Function.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Func<int[], int> delegateFunc = GetTheSmalletsNumber;
+            Console.WriteLine(delegateFunc(numbers));
+        }
+
+        static int GetTheSmalletsNumber(int[] numbers)
+        {
+            return numbers.Min();
+        }
+    }
+}
