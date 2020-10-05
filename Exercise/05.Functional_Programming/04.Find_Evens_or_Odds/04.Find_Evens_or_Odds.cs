5887587faa9f13b4479b06e44fd48af35d1da090
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-05 19:17:41 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/04.Find_Evens_or_Odds/04.Find_Evens_or_Odds.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Find_Evens_or_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersRange = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = numbersRange[0];
+            int end = numbersRange[1];
+            string type = Console.ReadLine();
+            Predicate<int> checker = IsNumberEven;
+
+            switch (type)
+            {
+                case "even":
+                    checker = IsNumberEven;
+                    break;
+                case "odd":
+                    checker = IsNumberOdd;
+                    break;
+            }
+
+            for (int currNumber = start; currNumber <= end; currNumber++)
+            {
+                if (checker(currNumber))
+                {
+                    Console.Write($"{currNumber} ");
+                }
+            }
+        }
+
+        static bool IsNumberEven(int number)
+        {
+            return number % 2 == 0;
+        }
+
+        static bool IsNumberOdd(int number)
+        {
+            return number % 2 != 0;
+        }
+    }
+}
