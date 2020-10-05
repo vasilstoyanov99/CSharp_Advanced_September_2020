5f3cf35ebf3cb9c6b58712f9fe093708bcaac7fa
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-05 20:08:37 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/06.Reverse_And_Exclude/06.Reverse_And_Exclude.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).Reverse().ToList();
+            int divider = int.Parse(Console.ReadLine());
+            List<int> result = RemoveNumbersThatAreOnlyDivisibleBy(divider, numbers);
+            Console.WriteLine(String.Join(" ", result));
+        }
+
+        static List<int> RemoveNumbersThatAreOnlyDivisibleBy(int divider, List<int> numbers)
+        {
+            return numbers.Where(x => x % divider != 0).ToList();
+        }
+    }
+}
