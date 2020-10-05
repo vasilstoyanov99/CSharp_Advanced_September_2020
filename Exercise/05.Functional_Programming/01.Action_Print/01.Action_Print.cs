e7260af49972098d1028735632c9f1eb1e391569
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-05 19:00:28 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/01.Action_Print/01.Action_Print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _01.Action_Print
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = Console.ReadLine().Split();
+            Action<string> printer = Print;
+
+            foreach (var name in names)
+            {
+                printer(name);
+            }
+        }
+
+        static void Print(string name)
+        {
+            Console.WriteLine(name);
+        }
+    }
+}
