b0f38cc887d35d9609e34fc46b746c333ff5aff7
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-06 16:39:39 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/09.List_Of_Predicates/09.List_Of_Predicates.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int upperBound = int.Parse(Console.ReadLine());
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            for (int currNumber = 1; currNumber <= upperBound; currNumber++)
+            {
+                if (CheckIfCurrNumIsDivisibleBy(numbers, currNumber))
+                {
+                    Console.Write($"{currNumber} ");
+                }
+            }
+        }
+
+        static bool CheckIfCurrNumIsDivisibleBy(int[] dividers, int currNum)
+        {
+            for (int i = 0; i < dividers.Length; i++)
+            {
+                int currDivider = dividers[i];
+
+                if (currNum % currDivider != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
