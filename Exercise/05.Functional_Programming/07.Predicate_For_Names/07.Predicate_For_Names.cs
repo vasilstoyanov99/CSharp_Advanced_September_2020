8d32fde6f21db5d7a376a289d0d2b76703961423
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-06 16:31:35 +0300

    Add done homework

---
+++ Exercise/05.Functional_Programming/07.Predicate_For_Names/07.Predicate_For_Names.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _07.Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenghtCriteria = int.Parse(Console.ReadLine());
+            string[] names = Console.ReadLine().Split();
+            
+            foreach (var name in names)
+            {
+                bool doesNameLenghMeetsCriteria = name.Length <= lenghtCriteria;
+
+                if (doesNameLenghMeetsCriteria)
+                {
+                    Console.WriteLine(name);
+                }
+            }
+
+            
+        }
+    }
+}
