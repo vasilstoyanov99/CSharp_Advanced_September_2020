7621713dfaf6641036b13776f56de9eae2dcda19
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-07 00:26:08 +0300

    Add done homework

---
+++ Lab/06.Defining_Classes/CarManufacturer/CarManufacturer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Tesla";
+            car.Model = "S";
+            car.Year = 2019;
+
+            Console.WriteLine($"Make: {car.Make}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Year: {car.Year}");
+        }
+    }
+}
