8358c0bae1c358ca3d942466f800071e9f35aa7b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-08 23:03:35 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/06.Speed_Racing/06.Speed_Racing.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Speed_Racing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfCars = int.Parse(Console.ReadLine());
+            Dictionary<string, Car> allCars = new Dictionary<string, Car>();
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string model = data[0];
+                double fuelAmount = double.Parse(data[1]);
+                double fuelConsumptionPerKilometer = double.Parse(data[2]);
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKilometer);
+                allCars.Add(model, car);
+            }
+
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                string[] command = input.Split();
+                string model = command[1];
+                double distance = double.Parse(command[2]);
+                allCars[model].Drive(distance);
+                input = Console.ReadLine();
+            }
+
+            foreach (var car in allCars.Values)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TravelledDistance}");
+            }
+        }
+    }
+}
---
+++ Exercise/06.Defining_Classes/06.Speed_Racing/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06.Speed_Racing
+{
+    class Car
+    {
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+        public double FuelConsumptionPerKilometer { get; set; }
+        public double TravelledDistance { get; set; }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+        public void Drive(double distance)
+        {
+            double neededFuel = FuelConsumptionPerKilometer * distance;
+
+            if (FuelAmount >= neededFuel)
+            {
+                FuelAmount -= neededFuel;
+                TravelledDistance += distance;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+    }
+}
