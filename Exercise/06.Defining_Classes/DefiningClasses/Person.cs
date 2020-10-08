761bc49db80c3752d3a0286653431a98553ea998
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-08 21:41:22 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/DefiningClasses/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DefiningClasses
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
