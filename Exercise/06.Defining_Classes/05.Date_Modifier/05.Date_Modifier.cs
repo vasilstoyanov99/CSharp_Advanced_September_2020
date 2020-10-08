346806795f65f709e59bc900396d8bda021cfbe1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-08 22:47:34 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/05.Date_Modifier/05.Date_Modifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+
+namespace _05.Date_Modifier
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+            DateTime startDate = DateTime.Parse(dateOne);
+            DateTime endDate = DateTime.Parse(dateTwo);
+            DateModifier dateModifier = new DateModifier();
+            dateModifier.GetDaysDifference(startDate, endDate);
+            Console.WriteLine(dateModifier.daysDifference);
+        }
+    }
+}
---
+++ Exercise/06.Defining_Classes/05.Date_Modifier/DateModifier.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.Date_Modifier
+{
+    class DateModifier
+    {
+        public int daysDifference { get; set; }
+
+        public void GetDaysDifference(DateTime startDate, DateTime endDate)
+        {
+            daysDifference = Math.Abs((startDate - endDate).Days);
+        }
+    }
+}
