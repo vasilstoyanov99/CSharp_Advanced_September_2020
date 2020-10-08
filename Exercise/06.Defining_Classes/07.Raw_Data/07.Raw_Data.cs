a7207447ecc99a7fbb4f161db88ead47d7c72764
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-08 23:49:39 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/07.Raw_Data/07.Raw_Data.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace _07.Raw_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> allCars = new List<Car>();
+            int numberOfCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCars; i++)
+            {
+                string[] rawData = Console.ReadLine().Split();
+                Car car = new Car(rawData);
+                allCars.Add(car);
+            }
+
+            string command = Console.ReadLine();
+            List<Car> sorted = new List<Car>();
+
+            switch (command)
+            {
+                case "fragile":
+                    sorted = allCars.Where(x => x.Cargo.Type == command && x.Tires.Any(x => x.Pressure < 1)).ToList();
+                    break;
+                case "flamable":
+                    sorted = allCars.Where(x => x.Cargo.Type == command && x.Engine.Power > 250).ToList();
+                    break;
+            }
+
+            foreach (var car in sorted)
+            {
+                Console.WriteLine(car.Model);
+            }
+        }
+    }
+}
---
+++ Exercise/06.Defining_Classes/07.Raw_Data/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace _07.Raw_Data
+{
+    class Car
+    {
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+        public string Model { get; set; }
+        public Car(string[] rawData)
+        {
+            string model = rawData[0];
+            Model = model;
+            int engineSpeed = int.Parse(rawData[1]);
+            int enginePower = int.Parse(rawData[2]);
+            AddEngineData(engineSpeed, enginePower);
+            int cargoWeight = int.Parse(rawData[3]);
+            string type = rawData[4];
+            AddCargoData(cargoWeight, type);
+            AddTiresData(rawData);
+        }
+
+        private void AddEngineData(int engineSpeed, int enginePower)
+        {
+            Engine engine = new Engine(engineSpeed, enginePower);
+            this.Engine = engine;
+        }
+
+        private void AddCargoData(int cargoWeight, string type)
+        {
+            Cargo cargo = new Cargo(cargoWeight, type);
+            this.Cargo = cargo;
+        }
+
+        private void AddTiresData(string[] rawData)
+        {
+            this.Tires = new Tire[3];
+            int index = 5;
+
+            for (int i = 0; i < 3; i++)
+            {
+                double pressure = double.Parse(rawData[index]);
+                int age = int.Parse(rawData[index + 1]);
+                index += 2;
+                Tire tire = new Tire(age, pressure);
+                this.Tires[i] = tire;
+            }
+        }
+    }
+}
