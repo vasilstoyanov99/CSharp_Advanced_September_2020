5cc7417dff4d3fd4ed0bf3b956ebd18700621198
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-08 22:07:54 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/04.Opinion_Poll/04.Opinion_Poll.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Opinion_Poll
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] data = Console.ReadLine().Split();
+                string name = data[0];
+                int age = int.Parse(data[1]);
+                Person person = new Person(name, age);
+                people.Add(person);
+            }
+
+            List<Person> sorted = people.Where(x => x.Age > 30).OrderBy(x => x.Name).ToList();
+
+            foreach (var person in sorted)
+            {
+                Console.WriteLine($"{person.Name} - {person.Age}");
+            }
+        }
+    }
+}
---
+++ Exercise/06.Defining_Classes/04.Opinion_Poll/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Opinion_Poll
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+}
