9255a0892c67868365344cee669e4fb2c214b876
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-09 15:49:13 +0300

    Add done homework

---
+++ Exercise/06.Defining_Classes/09.Pokemon_Trainer/09.Pokemon_Trainer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09.Pokemon_Trainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Trainer> allTraines = new Dictionary<string, Trainer>();
+            string input = Console.ReadLine();
+
+            while (input != "Tournament")
+            {
+                string[] data = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                AddNewTrainer(data, allTraines);
+                input = Console.ReadLine();
+            }
+
+            string secondInput = Console.ReadLine();
+
+            while (secondInput != "End")
+            {
+                string pokemonElement = secondInput;
+                CheckIfTrainerHasAPokemonWith(pokemonElement, allTraines);
+                secondInput = Console.ReadLine();
+            }
+
+            var sorted = allTraines.OrderByDescending(x => x.Value.Badges).ToDictionary(x => x.Key, y => y.Value);
+
+            foreach (var trainer in sorted)
+            {
+                Console.WriteLine($"{trainer.Key} {trainer.Value.Badges} {trainer.Value.PokemonCollection.Count}");
+            }
+        }
+
+        static void AddNewTrainer(string[] data, Dictionary<string, Trainer> allTraines)
+        {
+            string trainerName = data[0];
+            string pokemonName = data[1];
+            string pokemonElement = data[2];
+            int pokemonHealth = int.Parse(data[3]);
+
+            if (!allTraines.ContainsKey(trainerName))
+            {
+                Trainer trainer = new Trainer(trainerName);
+                allTraines.Add(trainerName, trainer);
+            }
+
+            Pokemon pokemon = new Pokemon(pokemonName, pokemonElement, pokemonHealth);
+            allTraines[trainerName].PokemonCollection.Add(pokemon);
+        }
+
+        static void CheckIfTrainerHasAPokemonWith(string element, Dictionary<string, Trainer> allTraines)
+        {
+            foreach (var trainer in allTraines)
+            {
+                if (trainer.Value.PokemonCollection.Any(x => x.Element == element))
+                {
+                    trainer.Value.Badges++;
+                }
+                else
+                {
+                    trainer.Value.ReducePokemonHealth();
+                }
+            }
+        }
+    }
+}
---
+++ Exercise/06.Defining_Classes/09.Pokemon_Trainer/Trainer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.Pokemon_Trainer
+{
+    class Trainer
+    {
+        public string Name { get; set; }
+        public int Badges { get; set; }
+        public List<Pokemon> PokemonCollection { get; set; }
+
+        public Trainer(string name)
+        {
+            Name = name;
+            Badges = 0;
+            PokemonCollection = new List<Pokemon>();
+        }
+
+        public void ReducePokemonHealth()
+        {
+            for (int i = 0; i < PokemonCollection.Count; i++)
+            {
+                PokemonCollection[i].Health -= 10;
+                CheckIfAPokimonIsDeadAndRemoveIt();
+            }
+        }
+
+        private void CheckIfAPokimonIsDeadAndRemoveIt()
+        {
+            for (int i = 0; i < PokemonCollection.Count; i++)
+            {
+                if (PokemonCollection[i].Health <= 0)
+                {
+                    PokemonCollection.Remove(PokemonCollection[i]);
+                    i--;
+                }
+            }
+        }
+    }
+}
