c0f86f7c97cf8f8d78d2e0e88e88b90bcb6dc909
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-10 22:16:21 +0300

    Add updates

---
+++ Lab/07.Implementing_Linked_List/02.Custom_Doubly_Linked_List/Linked_List.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Custom_Doubly_Linked_List
+{
+    public class Linked_List
+    {
+        public Node Head { get; set; }
+        public Node Tail { get; set; }
+
+        public void AddHead(Node node)
+        {
+            node.Next = Head;
+            Head = node;
+        }
+
+        public void AddTail(Node node)
+        {
+
+        }
+
+        public void PrintList()
+        {
+            Node currentNode = Head;
+
+            while (currentNode != null)
+            {
+                Console.WriteLine(currentNode.Value);
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
--- Lab/07.Implementing_Linked_List/02.Custom_Doubly_Linked_List/Node.cs
+++ Lab/07.Implementing_Linked_List/02.Custom_Doubly_Linked_List/Node.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace _01.Custom_Linked_List
+namespace _02.Custom_Doubly_Linked_List
 {
     public class Node
     {
         public int Value { get; set; }
-        public Node Next{ get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
 
         public Node(int value)
         {
