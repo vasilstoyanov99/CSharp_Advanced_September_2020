89c7b35dd25a896961df65f55c5e27762d07f13e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-13 23:32:26 +0300

    Add done 03.Generic_Scale task

---
+++ Lab/08.Generics/03.Generic_Scale/03.Generic_Scale.cs
@@ -0,0 +1,15 @@
+﻿using __GenericScale;
+using System;
+using System.Collections.Generic;
+
+namespace _GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<double> scale = new EqualityScale<double>(5.55, 5.55);
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
---
+++ Lab/08.Generics/03.Generic_Scale/EqualityScale.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace __GenericScale
+{
+    public class EqualityScale<T>
+    {
+        public T Left { get; set; }
+        public T Right { get; set; }
+
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
+    }
+}
