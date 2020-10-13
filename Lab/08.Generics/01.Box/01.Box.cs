f984b24a848bf92c986fdd3f4df19997268d8f5b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-13 23:18:01 +0300

    Add done 02.Array_Creator task

---
+++ Lab/08.Generics/01.Box/01.Box.cs
@@ -0,0 +1,22 @@
+﻿using _01.Box;
+using System;
+
+namespace _BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<int> box = new Box<int>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                box.Add(i);
+            }
+
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Remove());
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
---
+++ Lab/08.Generics/02.Array_Creator/02.Array_Creator.cs
@@ -0,0 +1,16 @@
+﻿using _02.Array_Creator;
+using System;
+
+namespace _StartUp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] money = ArrayCreator.Create(10, "$$$");
+            int[] repeat = ArrayCreator.Create(5, 15);
+            Console.WriteLine(String.Join(", ", money));
+            Console.WriteLine(String.Join(", ", repeat));
+        }
+    }
+}
