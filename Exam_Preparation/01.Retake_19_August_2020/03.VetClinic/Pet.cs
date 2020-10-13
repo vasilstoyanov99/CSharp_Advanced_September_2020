2278a28f631008b564b4732eb3e14250368e9117
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-13 23:06:04 +0300

    Add done 01.Box task

---
+++ Exam_Preparation/01.Retake_19_August_2020/03.VetClinic/Pet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.VetClinic
+{
+    public class Pet
+    {
+        string Name { get; set; }
+        string Owner { get; set; }
+        int Age { get; set; }
+        
+    }
+}
---
+++ Lab/08.Generics/01.Box/Box.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Box
+{
+    public class Box<T>
+    {
+        private Stack<T> stack;
+        public int Count { get; }
+
+        public Box()
+        {
+            stack = new Stack<T>();
+        }
+        public void Add(T element)
+        {
+            stack.Push(element);
+        }
+
+        public T Remove()
+        {
+           return stack.Pop();
+        }
+    }
+}
