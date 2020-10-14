de8484daa60b00ca64084f0490b6588438d9af4e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-14 23:14:12 +0300

    Add done 01. Generic Box of String task

---
+++ Exercise/09.Generics/01.Generic_Box_of_String/01.Generic_Box_of_String.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var value = Console.ReadLine();
+                Box<string> box = new Box<string>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+}
---
+++ Exercise/09.Generics/01.Generic_Box_of_String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Generic_Box_of_String
+{
+   public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}"; 
+        }
+    }
+}
