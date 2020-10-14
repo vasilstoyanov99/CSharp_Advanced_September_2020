4e430d52d044c04afc3e909367aa2616fd0605f6
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-14 17:39:28 +0300

    Add done 03.VetClinic task

---
+++ Exam_Preparation/01.Retake_19_August_2020/VetClinic/Clinic.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Clinic
+    {
+        public int Capacity { get; set; }
+        public int Count
+        {
+            get
+            {
+                return allPets.Count;
+            }
+        }
+        private Dictionary<string, Pet> allPets;
+
+        public Clinic(int capacity)
+        {
+            Capacity = capacity;
+            allPets = new Dictionary<string, Pet>(capacity);
+        }
+
+        public void Add(Pet pet)
+        {
+            if (allPets.Count <= 20)
+            {
+                allPets.Add(pet.Name, pet);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (allPets.ContainsKey(name))
+            {
+                allPets.Remove(name);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Pet GetPet(string name, string owner)
+        {
+            if (allPets.ContainsKey(name))
+            {
+                if (allPets[name].Owner == owner)
+                {
+                    return allPets[name];
+                }
+            }
+
+            return null;
+        }
+
+        public Pet GetOldestPet()
+        {
+            foreach (var oldestPet in allPets.Values.OrderByDescending(x => x.Age))
+            {
+                return oldestPet;
+            }
+
+            return null;
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append("The clinic has the following patients:");
+            result.AppendLine();
+
+            foreach (var pet in allPets.Values)
+            {
+                result.Append($"Pet {pet.Name} with owner: {pet.Owner}");
+                result.AppendLine();
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
--- Exam_Preparation/01.Retake_19_August_2020/VetClinic/StartUp.cs
+++ Exam_Preparation/01.Retake_19_August_2020/VetClinic/StartUp.cs
@@ -6,7 +6,22 @@ namespace VetClinic
     {
         static void Main(string[] args)
         {
-            
+            Clinic clinic = new Clinic(20);
+            Pet dog = new Pet("Ellias", 5, "Tim");
+            Console.WriteLine(dog);
+            clinic.Add(dog);
+            Console.WriteLine(clinic.Remove("Ellias"));
+            Console.WriteLine(clinic.Remove("Pufa"));
+            Pet cat = new Pet("Bella", 2, "Mia");
+            Pet bunny = new Pet("Zak", 4, "Jon");
+            clinic.Add(cat);
+            clinic.Add(bunny);
+            Pet oldestPet = clinic.GetOldestPet();
+            Console.WriteLine(oldestPet);
+            Pet pet = clinic.GetPet("Bella", "Mia");
+            Console.WriteLine(pet);
+            Console.WriteLine(clinic.Count);
+            Console.WriteLine(clinic.GetStatistics());
         }
     }
 }
