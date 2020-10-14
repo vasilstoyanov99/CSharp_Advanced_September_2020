4bedc6ee231da1c0219b53cb2148593b1fb6d3a8
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-14 15:26:52 +0300

    Add done 02.Custom_Stack task

---
+++ Lab/08.Create_Custom_Data_Structures/02.Custom_Stack/02.Custom_Stack .cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.Custom_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> myCustomStack = new CustomStack<int>();
+
+            for (int i = 1; i <= 5; i++)
+            {
+                myCustomStack.Push(i);
+            }
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.WriteLine($"Poped element: {myCustomStack.Pop()}");
+            }
+
+            Console.WriteLine("Print stack");
+            myCustomStack.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine();
+            Console.WriteLine($"Peeked element {myCustomStack.Peek()}");
+            Console.WriteLine($"Stack count {myCustomStack.Count}");
+            myCustomStack.Pop();
+            myCustomStack.Pop();
+            myCustomStack.Pop();
+        }
+    }
+}
---
+++ Lab/08.Create_Custom_Data_Structures/02.Custom_Stack/CustomStack.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _02.Custom_Stack
+{
+   public class CustomStack<T>
+    {
+        private T[] items;
+        private const int initialCapacity = 4;
+        public int Count { get; private set; }
+        public CustomStack()
+        {
+            items = new T[initialCapacity];
+            Count = 0;
+        }
+
+        public void Push(T element)
+        {
+            if (items.Length == Count)
+            {
+                Resize();
+            }
+
+            items[Count] = element;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (Count <= items.Length / 4)
+            {
+                Shrink();
+            }
+
+            T lastElement = items[Count - 1];
+            items[Count - 1] = default;
+            Count--;
+            return lastElement;
+        }
+
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            return items[Count - 1];
+        }
+
+        public void ForEach(Action<object> action)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                action(items[i]);
+            }
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[items.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+
+        private void Shrink()
+        {
+            T[] copy = new T[items.Length / 4];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = items[i];
+            }
+
+            items = copy;
+        }
+
+    }
+}
