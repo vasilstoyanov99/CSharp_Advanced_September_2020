3f5255cbfad540cfd5e98d2fbd7ddb97299f07cd
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 21:20:42 +0300

    Add done 002.Generic_Box_of_Integer task

---
+++ Exercise/09.Generics/02.Generic_Box_of_Integer/02.Generic_Box_of_Integer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.Generic_Box_of_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfInputs = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var value = int.Parse(Console.ReadLine());
+                Box<int> box = new Box<int>(value);
+                Console.WriteLine(box);
+            }
+        }
+    }
+    
+}
---
+++ Exercise/09.Generics/05.Generic_Count_Method_String/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.Generic_Count_Method_String
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
