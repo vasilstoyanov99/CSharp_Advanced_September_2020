d105b936bd45c2e5948b2b61af04ca10c757cd98
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 21:28:14 +0300

    Add done 06.Generic_Count_Method_Double task

---
+++ Exercise/09.Generics/06.Generic_Count_Method_Double/06.Generic_Count_Method_Double.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Generic_Count_Method_Double
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+            var allboxes = new List<Box<double>>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var value = double.Parse(Console.ReadLine());
+                Box<double> box = new Box<double>(value);
+                allboxes.Add(box);
+            }
+
+            var elementToCompare = double.Parse(Console.ReadLine());
+            Console.WriteLine(Compare(elementToCompare, allboxes));
+        }
+
+        static int Compare(double element, List<Box<double>> allBoxes)
+
+        {
+            int counter = 0;
+
+            foreach (var box in allBoxes)
+            {
+                if (box.Value.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
