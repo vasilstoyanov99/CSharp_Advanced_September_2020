56560fe5961252cb33500f36fa8554736446411b
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 21:22:39 +0300

    Add done 05.Generic_Count_Method_String task

---
+++ Exercise/09.Generics/05.Generic_Count_Method_String/05.Generic_Count_Method_String.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Generic_Count_Method_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+            var allboxes = new List<Box<string>>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var value = Console.ReadLine();
+                Box<string> box = new Box<string>(value);
+                allboxes.Add(box);
+            }
+
+            var elementToCompare = Console.ReadLine();
+            Console.WriteLine(Compare(elementToCompare, allboxes));
+        }
+
+        static int Compare(string element, List<Box<string>> allBoxes)
+           
+        {
+            int counter = 0;
+
+            foreach (var box in allBoxes)
+            {
+                if (box.Value.CompareTo(element) > 0)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+    }
+}
