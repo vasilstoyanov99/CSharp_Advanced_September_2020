6ce05f56d1e634413707f49eb22a7c6e95a83b70
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 22:32:24 +0300

    Add done 08.Threeuple task

---
+++ Exercise/09.Generics/08.Threeuple/08.Threeuple.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace _08.Threeuple
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string fistName = firstInput[0];
+            string lastName = firstInput[1];
+            string address = firstInput[2];
+            string town = String.Empty;
+
+            if (firstInput.Length == 5)
+            {
+                town = $"{firstInput[3]} {firstInput[4]}";
+            }
+            else
+            {
+                town = firstInput[3];
+            }
+            
+            string firstItem = $"{fistName} {lastName}";
+            Threeuple<string, string, string> fistThreeuple = new Threeuple<string, string, string>(firstItem, address, town);
+
+            string[] secondInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string name = secondInput[0];
+            int drankBeer = int.Parse(secondInput[1]);
+            string state = secondInput[2];
+            bool isDrunk = false;
+
+            if (state == "drunk")
+            {
+                isDrunk = true;
+            }
+            
+
+            Threeuple<string, int, bool> secondThreeuple = new Threeuple<string, int, bool>(name, drankBeer, isDrunk);
+
+            string[] thirdInput = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string personName = thirdInput[0];
+            double accountBalance = double.Parse(thirdInput[1]);
+            string bankName = thirdInput[2];
+            Threeuple<string, double, string> thirdThreeuple = new Threeuple<string, double, string>(personName, accountBalance, bankName);
+
+            Console.WriteLine(fistThreeuple);
+            Console.WriteLine(secondThreeuple);
+            Console.WriteLine(thirdThreeuple);
+        }
+    }
+}
---
+++ Exercise/09.Generics/08.Threeuple/Threeuple.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _08.Threeuple
+{
+    public class Threeuple<T, T2, T3>
+    {
+        public T FirstItem { get; set; }
+        public T2 SecondItem { get; set; }
+        public T3 ThirdItem { get; set; }
+
+        public Threeuple(T fisrtItem, T2 secondItem, T3 thirdItem)
+        {
+            FirstItem = fisrtItem;
+            SecondItem = secondItem;
+            ThirdItem = thirdItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem} -> {ThirdItem}";
+        }
+    }
+}
