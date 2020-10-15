dfaf3dfb990367c30150a00194b86c262776cf44
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 21:51:37 +0300

    Add done 07.Tuple task

---
+++ Exercise/09.Generics/07.Tuple/07.Tuple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] firstInput = Console.ReadLine().Split();
+            string fistName = firstInput[0];
+            string lastName = firstInput[1];
+            string address = firstInput[2];
+            string firstItem = $"{fistName} {lastName}";
+            Tuple<string, string> fistTuple = new Tuple<string, string>(firstItem, address);
+
+            string[] secondInput = Console.ReadLine().Split();
+            string name = secondInput[0];
+            int drinkingBeerCapacity = int.Parse(secondInput[1]);
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, drinkingBeerCapacity);
+
+            string[] thirdInput = Console.ReadLine().Split();
+            int thisIsInteger = int.Parse(thirdInput[0]);
+            double thisIsDouble = double.Parse(thirdInput[1]);
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(thisIsInteger, thisIsDouble);
+
+            Console.WriteLine(fistTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+        }
+    }
+}
---
+++ Exercise/09.Generics/07.Tuple/Tuple.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.Tuple
+{
+    public class Tuple<T, T2>
+    {
+        public T FirstItem { get; set; }
+        public T2 SecondItem { get; set; }
+        public Tuple(T firstItem, T2 secondItem)
+        {
+            FirstItem = firstItem;
+            SecondItem = secondItem;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstItem} -> {SecondItem}"; 
+        }
+    }
+}
