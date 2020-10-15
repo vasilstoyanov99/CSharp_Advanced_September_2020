26e0a3f1fb5c5fc7361feb06bffb6b3e3db2bf0a
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-15 21:22:10 +0300

    Add done 04.Generic_Swap_Method_Integer task

---
+++ Exercise/09.Generics/04.Generic_Swap_Method_Integer/04.Generic_Swap_Method_Integer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Generic_Swap_Method_Integer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberOfInputs = int.Parse(Console.ReadLine());
+            var allboxes = new List<Box<int>>();
+
+            for (int i = 0; i < numberOfInputs; i++)
+            {
+                var value = int.Parse(Console.ReadLine()); ;
+                Box<int> box = new Box<int>(value);
+                allboxes.Add(box);
+            }
+
+            var indexesToSwap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Swap(allboxes, indexesToSwap[0], indexesToSwap[1]);
+
+            foreach (var box in allboxes)
+            {
+                Console.WriteLine(box);
+            }
+
+        }
+
+        static void Swap(List<Box<int>> allBoxes, int fistIndex, int secondIndex)
+        {
+            var temp = allBoxes[fistIndex];
+            allBoxes[fistIndex] = allBoxes[secondIndex];
+            allBoxes[secondIndex] = temp;
+        }
+    }
+}
---
+++ Exercise/09.Generics/04.Generic_Swap_Method_Integer/Box.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Generic_Swap_Method_Integer
+{
+    public class Box<T>
+    {
+        public T Value { get; set; }
+
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
