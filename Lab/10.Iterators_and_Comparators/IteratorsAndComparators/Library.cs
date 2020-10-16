78ddd9f07d7a718fa15f0a5d592d11082c6ad60f
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-16 22:17:38 +0300

    Add done 01.Library task

---
+++ Lab/10.Iterators_and_Comparators/IteratorsAndComparators/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        public List<Book> Books { get; set; }
+
+        public Library(params Book[] books)
+        {
+            Books = books.ToList();
+        }
+        public IEnumerator<Book> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
