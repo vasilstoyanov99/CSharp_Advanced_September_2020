05e74bf8439df7fa79978bd90844cc421eff7176
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-20 18:19:45 +0300

    Add done 03.Guild task

---
+++ Exam_Preparation/03.22_Feb_2020/Guild/Guild.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Guild
+{
+    class Guild
+    {
+        private Dictionary<string, Player> allPlayers;
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+        public int Count 
+        { 
+            get
+            {
+                return allPlayers.Count;
+            }
+        }
+        public Guild(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            allPlayers = new Dictionary<string, Player>(Capacity);
+        }
+
+        public void AddPlayer(Player player)
+        {
+            if (allPlayers.Count <= Capacity)
+            {
+                allPlayers.Add(player.Name, player);
+            }
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            if (allPlayers.ContainsKey(name))
+            {
+                allPlayers.Remove(name);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void PromotePlayer(string name)
+        {
+            if (allPlayers[name].Rank != "Member")
+            {
+                allPlayers[name].Rank = "Member";
+            }
+        }
+
+        public void DemotePlayer(string name)
+        {
+            if (allPlayers[name].Rank != "Trial")
+            {
+                allPlayers[name].Rank = "Trial";
+            }
+        }
+
+        public Player[] KickPlayersByClass(string classToRemove)
+        {
+            List<Player> removedPlayers = new List<Player>();
+            removedPlayers = allPlayers.Values.Where(x => x.Class == classToRemove).ToList();
+            allPlayers = allPlayers.Where(x => x.Value.Class != classToRemove).ToDictionary(x => x.Key, y => y.Value);
+            return removedPlayers.ToArray();
+        } 
+        
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Players in the guild: {Name}");
+
+            foreach (var player in allPlayers.Values)
+            {
+                result.AppendLine(player.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam_Preparation/03.22_Feb_2020/Guild/Player.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guild
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public string Class { get; set; }
+        public string Rank { get; set; }
+        public string Description { get; set; }
+
+        public Player(string name, string classToInsert )
+        {
+            Name = name;
+            Class = classToInsert;
+            Rank = "Trial";
+            Description = "n/a";
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"Player {Name}: {Class}");
+            result.AppendLine($"Rank: {Rank}");
+            result.AppendLine($"Description: {Description}");
+            return result.ToString().TrimEnd(); 
+        }
+    }
+}
