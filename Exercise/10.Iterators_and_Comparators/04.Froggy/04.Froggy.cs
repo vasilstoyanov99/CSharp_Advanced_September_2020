67a7d7cf1d020bf2f83d68197647b952437a7b52
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-20 16:00:32 +0300

    Add done 04.Froggy task

---
+++ Exercise/10.Iterators_and_Comparators/04.Froggy/04.Froggy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Lake lake = new Lake(stones);
+            Console.WriteLine(String.Join(", ", lake));
+        }
+    }
+}
---
+++ Exercise/10.Iterators_and_Comparators/04.Froggy/Lake.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    public class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] items)
+        {
+            stones = items;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            for (int i = 0; i < stones.Length; i += 2)
+            {
+                yield return stones[i];
+            }
+
+            for (int i = stones.Length - 1; i > 0; i--)
+            {
+                if (i % 2 == 1)
+                {
+                    yield return stones[i];
+                }
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
