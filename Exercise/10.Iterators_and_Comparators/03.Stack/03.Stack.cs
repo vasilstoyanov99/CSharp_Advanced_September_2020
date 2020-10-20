3f0f4916a80db4ae0a236d001e5f38012fd33311
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-20 15:34:52 +0300

    Add done 03.Stack task

---
+++ Exercise/10.Iterators_and_Comparators/03.Stack/03.Stack.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<int> myCustomStack = new CustomStack<int>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                List<string> command = input.Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                switch (command[0])
+                {
+                    case "Push":
+                        int[] numbers = command.Skip(1).Select(int.Parse).ToArray();
+
+                        foreach (var number in numbers)
+                        {
+                            myCustomStack.Push(number);
+                        }
+
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            myCustomStack.Pop();
+                        }
+                        catch (InvalidOperationException msg)
+                        {
+                            Console.WriteLine(msg.Message);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+
+            if (myCustomStack.Count > 0)
+            {
+                Console.WriteLine(String.Join(Environment.NewLine, myCustomStack));
+                Console.WriteLine(String.Join(Environment.NewLine, myCustomStack));
+            }
+
+
+        }
+    }
+}
---
+++ Exercise/10.Iterators_and_Comparators/03.Stack/CustomStack.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.Stack
+{
+    class CustomStack<T> : IEnumerable<T>
+    {
+        private T[] elements;
+        private const int initialCapacity = 4;
+        public int Count { get; private set; }
+        public CustomStack()
+        {
+            elements = new T[initialCapacity];
+        }
+
+        public void Push(T element)
+        {
+            if (elements.Length == Count)
+            {
+                Resize();
+            }
+
+            elements[Count] = element;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("No elements");
+            }
+
+
+            if (Count <= elements.Length / 4)
+            {
+                Shrink();
+            }
+
+            T lastElement = elements[Count - 1];
+            elements[Count - 1] = default;
+            Count--;
+            return lastElement;
+        }
+
+        private void Resize()
+        {
+            T[] copy = new T[elements.Length * 2];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = elements[i];
+            }
+
+            elements = copy;
+        }
+
+        private void Shrink()
+        {
+            T[] copy = new T[elements.Length / 4];
+
+            for (int i = 0; i < Count; i++)
+            {
+                copy[i] = elements[i];
+            }
+
+            elements = copy;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = Count - 1; i >= 0; i--)
+            {
+                yield return elements[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
