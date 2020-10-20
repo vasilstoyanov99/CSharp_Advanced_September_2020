cdd603a6269efc1f437c27503b787dbfaeec0844
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-20 13:09:52 +0300

    Add done 01.ListyIterator task

---
+++ Exercise/10.Iterators_and_Comparators/01.ListyIterator/01.ListyIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> rawElements = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            rawElements.RemoveAt(0);
+
+            if (rawElements.Count == 0)
+            {
+                rawElements = null;
+            }
+            
+            ListyIterator<string> listyIterator = new ListyIterator<string>(rawElements);
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] command = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "Move":
+                        Console.WriteLine(listyIterator.Move());
+                        break;
+                    case "HasNext":
+                        Console.WriteLine(listyIterator.HasNext());
+                        break;
+                    case "Print":
+                        Console.WriteLine(listyIterator.Print());
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Exercise/10.Iterators_and_Comparators/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.ListyIterator
+{
+    public class ListyIterator<T>
+    {
+        private List<T> list;
+        private int internalIndex;
+
+        public ListyIterator(List<T> elements)
+        {
+            list = elements;
+            internalIndex = 0;
+        }
+
+        public bool Move()
+        {
+            if (internalIndex + 1 == list.Count)
+            {
+                return false;
+            }
+            else
+            {
+                internalIndex++;
+                return true;
+            }
+        }
+
+        public bool HasNext()
+        {
+            if (internalIndex + 1 == list.Count)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public string Print()
+        {
+            if (list == null)
+            {
+                throw new Exception("Invalid Operation!");
+            }
+
+            return list[internalIndex].ToString();
+        }
+    }
+}
