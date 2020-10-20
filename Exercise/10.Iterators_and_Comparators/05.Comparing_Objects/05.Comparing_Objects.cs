d7b5097435764694da386678c768fbcb2255dd7e
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-20 16:26:25 +0300

    Add done 05.Comparing_Objects task

---
+++ Exercise/10.Iterators_and_Comparators/05.Comparing_Objects/05.Comparing_Objects.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Comparing_Objects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            List<Person> allPeople = new List<Person>();
+
+            while (input != "END")
+            {
+                string[] personData = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string name = personData[0];
+                int age = int.Parse(personData[1]);
+                string town = personData[2];
+                Person person = new Person(name, age, town);
+                allPeople.Add(person);
+                input = Console.ReadLine();
+            }
+
+            int number = int.Parse(Console.ReadLine());
+            Person toBeCompared = allPeople[number - 1];
+            int samePeopleCounter = 0;
+
+            foreach (var person in allPeople)
+            {
+                if (person.CompareTo(toBeCompared) == 0)
+                {
+                    samePeopleCounter++;
+                }
+            }
+
+            if (samePeopleCounter == 1)
+            {
+                Console.WriteLine("No matches");
+            }
+            else
+            {
+                Console.WriteLine($"{samePeopleCounter} {allPeople.Count - samePeopleCounter} {allPeople.Count}");
+            }
+        }
+    }
+}
---
+++ Exercise/10.Iterators_and_Comparators/05.Comparing_Objects/Person.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.Comparing_Objects
+{
+    public class Person : IComparable<Person>
+    {
+        private string name;
+        private int age;
+        private string town;
+
+        public Person(string name, int age, string town)
+        {
+            this.name = name;
+            this.age = age;
+            this.town = town;
+        }
+
+        public int CompareTo(Person other)
+        {
+            int result = 1;
+
+            if (other == null)
+            {
+                return result;
+            }
+
+            result = this.name.CompareTo(other.name);
+
+            if (result == 0)
+            {
+                result = this.age.CompareTo(other.age);
+
+                if (result == 0)
+                {
+                    result = this.town.CompareTo(other.town);
+                }
+            }
+
+            return result;
+        }
+    }
+}
