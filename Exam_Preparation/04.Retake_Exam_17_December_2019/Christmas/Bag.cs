6cd71a4c800ac5b649bbf9fd934299c0a416d3da
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-22 12:06:14 +0300

    Add done 03.Christmas task

---
+++ Exam_Preparation/04.Retake_Exam_17_December_2019/Christmas/Bag.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Christmas
+{
+   public class Bag
+    {
+        private Dictionary<string, Present> allPresents;
+        public string Color { get; set; }
+        public int Capacity { get; set; }
+
+        public int Count 
+        {
+            get 
+            {
+                return allPresents.Count;    
+            } 
+        }
+        public Bag(string color, int capacity)
+        {
+            Color = color;
+            Capacity = capacity;
+            allPresents = new Dictionary<string, Present>();
+        }
+
+        public void Add(Present present)
+        {
+            if (allPresents.Count <= Capacity)
+            {
+                allPresents.Add(present.Name, present);
+            }
+        }
+
+        public bool Remove(string name)
+        {
+            if (allPresents.ContainsKey(name))
+            {
+                allPresents.Remove(name);
+                return true;
+            }
+            return false;
+        }
+
+        public Present GetHeaviestPresent()
+        {
+            foreach (var heaviestPresent in allPresents.Values.OrderByDescending(x => x.Weight))
+            {
+                return heaviestPresent;
+            }
+
+            return null;
+        }
+
+        public Present GetPresent(string name)
+        {
+            if (allPresents.ContainsKey(name))
+            {
+                return allPresents[name];
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            StringBuilder result = new StringBuilder();
+            result.AppendLine($"{Color} bag contains:");
+
+            foreach (var present in allPresents.Values)
+            {
+                result.AppendLine(present.ToString());
+            }
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exam_Preparation/04.Retake_Exam_17_December_2019/Christmas/Present.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Christmas
+{
+    public class Present
+    {
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public string Gender { get; set; }
+
+        public Present(string name, double weight, string gender)
+        {
+            Name = name;
+            Weight = weight;
+            Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Present {Name} ({Weight}) for a {Gender}"; 
+        }
+    }
+}
