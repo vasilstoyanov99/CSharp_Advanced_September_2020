1240949bda81285aeb07778aa93b4d55b090d707
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-28 16:03:35 +0200

    Add done 04.Random_List task

---
+++ OOP/Lab/01.Inheritance/CustomRandomList/04.Random_List.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList list = new RandomList();
+
+            list.Add("T");
+            list.Add("E");
+            list.Add("S");
+            list.Add("T");
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
