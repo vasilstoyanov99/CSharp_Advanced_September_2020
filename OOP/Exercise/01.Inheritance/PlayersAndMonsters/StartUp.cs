1376e0af36dbc9142c49c01130e0f76a424c7619
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-28 20:11:46 +0200

    Add done 03.Players_and_Monsters task

--- OOP/Exercise/01.Inheritance/PlayersAndMonsters/StartUp.cs
+++ OOP/Exercise/01.Inheritance/PlayersAndMonsters/StartUp.cs
@@ -1,10 +1,13 @@
-﻿namespace PlayersAndMonsters
+﻿using System;
+
+namespace PlayersAndMonsters
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-
+            BladeKnight bladeKnight = new BladeKnight("SuckDick", 69420);
+            Console.WriteLine(bladeKnight);
         }
     }
 }
