af0ebb824e78f8c786d149baff63a94f66a57447
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-28 16:28:01 +0200

    Add done 05.Stack_of_Strings task

---
+++ OOP/Lab/01.Inheritance/CustomStack/05.Stack_of_Strings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stack = new StackOfStrings();
+            Console.WriteLine(stack.IsEmpty());
+            stack.Push("Fist");
+            stack.Push("Second");
+            stack.Push("Third");
+            Console.WriteLine(stack.IsEmpty());
+            Stack<string> toAdd = new Stack<string>(new string[] { "Hi", "Hello", "What's up" });
+            stack.AddRange(toAdd);
+            
+        }
+    }
+}
---
+++ OOP/Lab/01.Inheritance/CustomStack/StackOfStrings.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return this.Count == 0;
+        }
+
+        public void AddRange(Stack<string> stack)
+        {
+            while (stack.Any())
+            {
+                this.Push(stack.Pop());
+            }
+        }
+    }
+}
