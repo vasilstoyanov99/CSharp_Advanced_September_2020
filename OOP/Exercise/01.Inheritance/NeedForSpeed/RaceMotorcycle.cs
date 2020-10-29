2c980397512e6b3ba6e5fba1f4d6fd0ae67d9de2
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-29 14:41:56 +0200

    Add done 04.Need_for_Speed task

---
+++ OOP/Exercise/01.Inheritance/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel)
+            :base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 8;
+        }
+
+        public override void Drive(double kilometers)
+        {
+            double usedFuel = DefaultFuelConsumption * kilometers;
+            Fuel -= usedFuel;
+        }
+    }
+}
---
+++ OOP/Exercise/01.Inheritance/NeedForSpeed/SportCar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    public class SportCar : Car
+    {
+        public SportCar(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 10;
+        }
+
+        public override void Drive(double kilometers)
+        {
+            double usedFuel = DefaultFuelConsumption * kilometers;
+            Fuel -= usedFuel;
+        }
+    }
+}
