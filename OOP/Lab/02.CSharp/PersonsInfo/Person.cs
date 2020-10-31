6b2fddba1af0380b5fc939134431ac5e13c5120f
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-10-31 13:28:26 +0200

    Update task to match 02.Salary requirements

--- OOP/Lab/02.CSharp/PersonsInfo/Person.cs
+++ OOP/Lab/02.CSharp/PersonsInfo/Person.cs
@@ -12,16 +12,30 @@ namespace PersonsInfo
 
         public int Age { get; private set; }
 
-        public Person(string firstName, string lastName, int age)
+        public decimal Salary { get; private set; }
+        public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
             LastName = lastName;
             Age = age;
+            Salary = salary;
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age > 30)
+            {
+                Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                Salary += Salary * percentage / 200;
+            }
         }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName} is {Age} years old."; 
+            return $"{FirstName} {LastName} receives {Salary:f2} leva."; 
         }
     }
 }
