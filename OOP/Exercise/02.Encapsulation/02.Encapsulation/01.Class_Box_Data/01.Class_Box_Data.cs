4896328ca80b2902d50fecf91ecfde3c88832120
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-02 16:49:18 +0200

    Add done 01.Class_Box_Data task

---
+++ OOP/Exercise/02.Encapsulation/02.Encapsulation/01.Class_Box_Data/01.Class_Box_Data.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _01.Class_Box_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            try
+            {
+                Box tryTo = new Box(length, width, height);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            Box box = new Box(length, width, height);
+            Console.WriteLine($"Surface Area - {box.CalculateTheSurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.CalculateTheLateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.CalculateTheVolume():f2}");
+        }
+    }
+}
---
+++ OOP/Exercise/02.Encapsulation/02.Encapsulation/01.Class_Box_Data/Box.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01.Class_Box_Data
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double CalculateTheSurfaceArea()
+        {
+            double result = (2 * Length * Width) + (2 * Length * Height) + (2 * Width * Height);
+            return result;
+        }
+
+        public double CalculateTheLateralSurfaceArea()
+        {
+            double result = (2 * Length * Height) + (2 * Width * Height);
+            return result;
+        }
+
+        public double CalculateTheVolume()
+        {
+            double result = Length * Width * Height;
+            return result;
+        }
+    }
+}
