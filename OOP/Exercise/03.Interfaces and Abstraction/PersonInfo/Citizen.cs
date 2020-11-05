81648949d4e2dea4103292d46ee49591ffe1ef01
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-05 21:38:24 +0200

    Update task to match 02. Multiple Implementation requirements

--- OOP/Exercise/03.Interfaces and Abstraction/PersonInfo/Citizen.cs
+++ OOP/Exercise/03.Interfaces and Abstraction/PersonInfo/Citizen.cs
@@ -4,15 +4,19 @@ using System.Text;
 
 namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string id, string birthdate)
         {
             Name = name;
             Age = age;
+            Id = id;
+            Birthdate = birthdate;
         }
 
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Id { get; set; }
+        public string Birthdate { get; set; }
     }
 }
