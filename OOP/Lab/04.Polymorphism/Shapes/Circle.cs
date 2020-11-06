b8c3becc35eff5c3e37f37f3ac545ffbb6989bf5
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-06 23:09:05 +0200

    Add done 03.Shapes task

---
+++ OOP/Lab/04.Polymorphism/Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public override string Draw()
+        {
+            StringBuilder result = new StringBuilder();
+            double rIn = this.radius - 0.4;
+            double rOut = this.radius + 0.4;
+            for (double y = this.radius; y >= -this.radius; --y)
+            {
+                for (double x = -this.radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                        result.Append("*");
+                    else
+                        result.Append(" ");
+                }
+                result.AppendLine();
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ OOP/Lab/04.Polymorphism/Shapes/Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Shapes
+{
+    public class Rectangle : Shape
+    {
+        private double length;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            this.length = height;
+            this.width = width;
+        }
+        public override double CalculateArea()
+        {
+            return length * width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * (length + width);
+        }
+
+        public override string Draw()
+        {
+            StringBuilder result = new StringBuilder();
+            DrawLine(this.width, '*', '*', result);
+            for (int i = 1; i < this.length - 1; ++i)
+                DrawLine(this.width, '*', ' ', result);
+            DrawLine(this.width, '*', '*', result);
+            return result.ToString();
+        }
+
+        private void DrawLine(double width, char end, char mid, StringBuilder result)
+        {
+            result.Append(end);
+            for (int i = 1; i < width - 1; ++i)
+                result.Append(mid);
+            result.AppendLine(end.ToString());
+        }
+    }
+}
