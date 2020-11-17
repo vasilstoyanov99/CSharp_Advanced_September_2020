bded2424c6cba513aa215ba79a7057775354b36f
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-17 22:58:38 +0200

    Add done 01.Stealer task

---
+++ OOP/Lab/07.Reflection_and_Attributes/07.Reflection_and_Attributes/StartUp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stealer
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ OOP/Lab/07.Reflection_and_Attributes/Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string className, params string[] filedsNames)
+        {
+            var classType = Type.GetType(className);
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+            var result = new StringBuilder();
+            result.AppendLine($"Class under investigation: {className}");
+
+            foreach (var filedName in filedsNames)
+            {
+                var fieldInfo = classType.GetField(filedName, 
+                    BindingFlags.Public
+                    | BindingFlags.NonPublic 
+                    | BindingFlags.Instance 
+                    | BindingFlags.Static 
+                    | BindingFlags.Instance);
+                result.AppendLine($"{filedName} = {fieldInfo.GetValue(classInstance)}");
+            }
+
+           return result.ToString().TrimEnd();
+        }
+    }
+}
