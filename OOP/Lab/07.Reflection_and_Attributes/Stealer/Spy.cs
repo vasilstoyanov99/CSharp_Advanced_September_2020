5110f353c9225f8f3cab20c21bc93dd3197fe6a1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-18 13:21:23 +0200

    Update task to match 02. High Quality Mistakes requirements

--- OOP/Lab/07.Reflection_and_Attributes/Stealer/Spy.cs
+++ OOP/Lab/07.Reflection_and_Attributes/Stealer/Spy.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Text;
 
@@ -26,6 +27,34 @@ namespace Stealer
             }
 
            return result.ToString().TrimEnd();
+        }
+
+        public string AnalyzeAcessModifiers(string className)
+        {
+            var classType = Type.GetType(className);
+            FieldInfo[] classFileds = classType.GetFields(BindingFlags.Public
+                | BindingFlags.Instance
+                | BindingFlags.Static);
+            MethodInfo[] classPublicMethods = classType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+            MethodInfo[] classNonPublicMethods = classType.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance);
+            StringBuilder result = new StringBuilder();
+
+            foreach (var filed in classFileds)
+            {
+                result.AppendLine($"{filed.Name} must be private!");
+            }
+
+            foreach (var method in classNonPublicMethods.Where(n => n.Name.StartsWith("get")))
+            {
+                result.AppendLine($"{method.Name} have to be public!");
+            }
+
+            foreach (var method in classPublicMethods.Where(n => n.Name.StartsWith("set")))
+            {
+                result.AppendLine($"{method.Name} have to be private!");
+            }
+
+            return result.ToString().TrimEnd();
         }
     }
 }
