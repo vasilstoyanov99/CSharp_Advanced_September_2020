a091d09dd51ebfd93c38fa4de0e1ac4085f18d5c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-18 15:11:43 +0200

    Update task to match 06. Code Tracker requirements

--- OOP/Lab/07.Reflection_and_Attributes/AuthorProblem/Program.cs
+++ OOP/Lab/07.Reflection_and_Attributes/AuthorProblem/Program.cs
@@ -3,12 +3,13 @@
 namespace AuthorProblem
 {
     [Author("Ventisi")]
-     class StartUp
+     public class StartUp
     {
         [Author("Gosho")]
         static void Main(string[] args)
         {
-            
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
         }
     }
 }
---
+++ OOP/Lab/07.Reflection_and_Attributes/AuthorProblem/Tracker.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            var type = typeof(StartUp);
+            MethodInfo[] classMethods = type.GetMethods(BindingFlags.Instance
+                | BindingFlags.Public
+                | BindingFlags.Static);
+
+            foreach (var method in classMethods)
+            {
+                if (method.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var customAttributes = method.GetCustomAttributes(false);
+
+                    foreach (AuthorAttribute attr in customAttributes)
+                    {
+                        Console.WriteLine($"{method.Name} is written by {attr.Name}"); 
+                    }
+                }
+            }
+        }
+    }
+}
