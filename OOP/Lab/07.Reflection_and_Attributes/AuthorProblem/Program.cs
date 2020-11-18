a004e1a783e15ef4929e55107f3986f6e9540686
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-18 14:57:31 +0200

    Add done 05. Create Attribute task

---
+++ OOP/Lab/07.Reflection_and_Attributes/AuthorProblem/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Ventisi")]
+     class StartUp
+    {
+        [Author("Gosho")]
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
