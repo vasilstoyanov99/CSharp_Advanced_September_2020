14c89c85989bda9e130871513eb6435653278925
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-19 22:57:46 +0200

    Add done 02. Validation Attributes task

---
+++ OOP/Exercise/04.ReflectionAndAttributes/ValidationAttributes/Attributes/MyRangeAttribute.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ValidationAttributes.Attributes
+{
+    public class MyRangeAttribute : MyValidationAttribute
+    {
+        private int min;
+        private int max;
+
+        public MyRangeAttribute(int min, int max)
+        {
+            this.min = min;
+            this.max = max;
+        }
+
+        public override bool IsValid(object obj)
+        {
+            if (!(obj is int))
+            {
+                throw new ArgumentException("The age shoud be an integer!");
+            }
+
+            int ageArg = (int)(obj);
+            return (ageArg >= min && ageArg <= max);
+        }
+    }
+}
---
+++ OOP/Exercise/04.ReflectionAndAttributes/ValidationAttributes/Models/Validator.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public static class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            PropertyInfo[] propertyInfo = obj.GetType().GetProperties();
+
+            foreach (var property in propertyInfo)
+            {
+                var propertyCustomAttributes = property.GetCustomAttributes<MyValidationAttribute>();
+
+                foreach (var attribute in propertyCustomAttributes)
+                {
+                    bool result = attribute.IsValid(property.GetValue(obj));
+
+                    if (!result)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
