e9ca82e179bf45fef7124594e3e314b2265228e1
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2020-11-21 15:00:27 +0200

    Add done 02. Dummy Tests task

--- OOP/Lab/09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
+++ OOP/Lab/09. CSharp-OOP-Unit-Testing-Lab-Skeleton/Skeleton.Tests/DummyTests.cs
@@ -4,5 +4,54 @@ using System;
 [TestFixture]
 public class DummyTests
 {
-   
+    [Test]
+     public void Dummy_Should_Lose_Health_When_Attacked()
+     {
+         // Arrange
+         Axe axe = new Axe(1, 10);
+         Dummy dummy = new Dummy(10, 10);
+
+         // Act
+         axe.Attack(dummy);
+         int expectedResult = 9;
+         int actualResult = dummy.Health;
+
+         // Assert
+         Assert.AreEqual(expectedResult, actualResult);
+     }
+
+     [Test]
+     public void Dead_Dummy_Should_Throw_Invalid_Operation_Exception_When_Attacked()
+     {
+         // Arrange
+         Axe axe = new Axe(1, 10);
+         Dummy dummy = new Dummy(0, 10);
+
+         // Act - Assert
+         Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+     }
+
+     [Test]
+     public void Dead_Dummy_Should_Give_XP()
+     {
+         // Arrange
+         Dummy dummy = new Dummy(0, 10);
+
+         // Act
+         int expectedResult = 10;
+         int actualResult = dummy.GiveExperience();
+
+         // Assert
+         Assert.AreEqual(expectedResult, actualResult);
+     }
+
+     [Test]
+     public void Alive_Dummy_Should_Not_Give_XP_It_Shold_Throw_Invalid_Operation_Exception()
+     {
+         // Arrange
+         Dummy dummy = new Dummy(10, 10);
+
+         // Act - Assert
+         Assert.Throws<InvalidOperationException>(() => dummy.GiveExperience());
+     }
 }
